059cd7351fc4287693b5c92466706e81a37ebe56
Julia Babicheva <julia.bbchv@gmail.com>
2025-03-11 16:19:47 -0700

    add footer search/icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,25 @@
 import vol from "../../assets/icons/sound.svg";
 import "./Footer.scss";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import musicIcon from "../../assets/icons/music.svg";
+import noteIcon from "../../assets/icons/note.svg";
+import searchIcon from "../../assets/icons/search.svg";
 
 export default function Footer() {
   const [currentDateTime, setCurrentDateTime] = useState<Date>(new Date());
+  const [selectedApp, setSelectedApp] = useState<string | null>("noteApp");
+
+  const handleAppSelect = (appId: string) => {
+    setSelectedApp(appId);
+  };
+
+  const isAppSelected = (appId: string) => {
+    return selectedApp === appId;
+  };
+
+  const getAppClasses = (appId: string) => {
+    return `footer__app ${isAppSelected(appId) ? `footer__app--selected` : ``}`;
+  };
 
   const formatTime = (date: Date): string => {
     const hours = String(date.getHours()).padStart(2, "0");
@@ -28,6 +44,29 @@ export default function Footer() {
 
   return (
     <footer className="footer">
+      <div className="footer__apps">
+        <div className="footer__search">
+          <div className="footer__search-wrapper">
+            <img
+              className="footer__search-icon"
+              src={searchIcon}
+              alt="search icon"
+            />
+          </div>
+        </div>
+        <div
+          className={getAppClasses("musicApp")}
+          onClick={() => handleAppSelect("musicApp")}
+        >
+          <img src={musicIcon} alt="music icon" />
+        </div>
+        <div
+          className={getAppClasses("noteApp")}
+          onClick={() => handleAppSelect("noteApp")}
+        >
+          <img src={noteIcon} alt="note icon" />
+        </div>
+      </div>
       <div className="footer__wrapper">
         <div className="footer__vol">
           <img className="footer__vol-icon" src={vol} alt="volume icon" />
