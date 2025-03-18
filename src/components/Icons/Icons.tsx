59c2b28999185eaea15a837ba185af56a5e37c7e
Julia Babicheva <julia.bbchv@gmail.com>
2025-03-18 11:44:09 -0700

    add folder component

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -3,18 +3,18 @@ import folder from "../../assets/icons/folder.svg";
 import phone from "../../assets/icons/phone.svg";
 import "./Icons.scss";
 
-export default function Icons() {
+export default function Icons({ handleOpen }: any) {
   return (
     <section className="icons">
-      <div className="icon">
+      <div className="icon" onClick={() => handleOpen("webDev")}>
         <img className="icon__img" src={computer} alt="computer icon" />
         <p className="icon__text">Web Dev</p>
       </div>
-      <div className="icon">
+      <div className="icon" onClick={() => handleOpen("socialMedia")}>
         <img className="icon__img" src={folder} alt="folder icon" />
         <p className="icon__text">Social Media</p>
       </div>
-      <div className="icon">
+      <div className="icon" onClick={() => handleOpen("contactMe")}>
         <img className="icon__img" src={phone} alt="phone icon" />
         <p className="icon__text">Contact Me</p>
       </div>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,28 @@
 import "./HomePage.scss";
+import { useState } from "react";
 import Icons from "../../components/Icons/Icons.tsx";
 import Note from "../../components/Note/Note.tsx";
 import Footer from "../../components/Footer/Footer.tsx";
+import Folder from "../../components/Folder/Folder.tsx";
 
 export default function HomePage() {
+  const [selectedApp, setSelectedApp] = useState<string>("");
+
+  const handleOpen = async (iconName: string) => {
+    setSelectedApp(iconName);
+  };
+
+  const isAppSelected = (iconName: string) => {
+    return selectedApp === iconName;
+  };
+
   return (
     <>
       <main className="home-main">
         <div className="home-main__wrapper">
-          <Icons />
-          <Note />
+          <Icons handleOpen={(iconName: string) => handleOpen(iconName)} />
+          <Note selectedApp={selectedApp} />
+          <Folder selectedApp={selectedApp} setSelectedApp={setSelectedApp} />
         </div>
       </main>
       <Footer />
